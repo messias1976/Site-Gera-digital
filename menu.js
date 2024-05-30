02e9bc44d2ca17d94c4a45d8138f0e809064009f
Messias Machado <messiasmachado1976@gmail.com>
2024-05-30 10:21:41 -0300

    auteração do site
    habilitação do menu, links e identidade do site

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
+
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir')
 })
@@ -13,3 +14,24 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir')
 })
+
+
+let calcSrollValue = () => {
+    let scrollProgress = document.getElementById("scrolltop");
+    let progressValue = document.getElementById("icone");
+    let pos = document.documentElement.scrollTop;
+    let calcHeigth =
+    document.documentElement.scrollHeight-document.documentElement.clientHeight;
+    let scrollValue = Math.round ((pos * 100) / calcHeigth);
+    if (pos > 100){
+        scrollProgress.style.display = "grid";
+    }else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+};
+
+window.onscroll = calcSrollValue;
+window.onload = calcSrollValue;
